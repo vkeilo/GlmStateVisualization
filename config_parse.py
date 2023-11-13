c35c23136859d7ac0803952c4093e3842e4720fb
vkeilo <yecup1056@gmail.com>
2023-11-13 23:28:43 +0800

    翻译任务数据成功生成

--- config_parse.py
+++ config_parse.py
@@ -6,9 +6,12 @@ class VisualConfig():
     def __init__(self, config_path = "./visual_config.json"):
         self.config = self.get_config(config_path)
         self.save_dir_path = self.config['save_dir_path']
+        self.ori_hidden_state_path = self.config['ori_hidden_state_path']
         self.tmp_dir_path = self.config["tmp_dir_path"]
         self.THUDM_dir_path = self.config["THUDM_dir_path"]
         self.role_setting_prompt_json = self.config["role_setting_prompt_json"]
+        self.text_prompt_data_path = self.config["text_prompt_data_path"]
+
     def get_config(self, file_name):
         with open(file_name, 'r', encoding='utf-8') as f:
             config = json.load(f)
