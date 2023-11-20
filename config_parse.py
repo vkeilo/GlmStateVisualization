f801c2d1fd6813c1f71f127217c2ba8945057a17
vkeilo <yecup1056@gmail.com>
2023-11-20 23:30:49 +0800

    增加counter任务分析

--- config_parse.py
+++ config_parse.py
@@ -7,7 +7,7 @@ class VisualConfig():
         self.config = self.get_config(config_path)
         self.save_dir_path = self.config['save_dir_path']
         self.ori_hidden_state_path = self.config['ori_hidden_state_path']
-        self.tmp_dir_path = self.config["tmp_dir_path"]
+        # self.tmp_dir_path = self.config["tmp_dir_path"]
         self.THUDM_dir_path = self.config["THUDM_dir_path"]
         self.role_setting_prompt_json = self.config["role_setting_prompt_json"]
         self.text_prompt_data_path = self.config["text_prompt_data_path"]
